42c90ebe40ead4430af90726219c66517ab46d57
Matheus-Henrique1 <m_peu@hotmail.com>
2023-05-08 21:54:17 -0300

    Game seed

--- src/main/java/com/devfocado/dslist/entities/Game.java
+++ src/main/java/com/devfocado/dslist/entities/Game.java
@@ -20,9 +20,14 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
 }
