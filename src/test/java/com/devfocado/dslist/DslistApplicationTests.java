db208b25e63cc46c4c1e0236a2c5fb7391288fe7
Matheus-Henrique1 <m_peu@hotmail.com>
2023-05-08 21:23:23 -0300

    Projeto criado

---
+++ src/test/java/com/devfocado/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devfocado.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
